bd154683dac128b6743a5c3029c76ab8ea271091
Ho Hai Dang <hohaidang.khtn@gmail.com>
2021-01-04 17:19:38 +0900

    add

--- VSCodeDebug/main.cpp
+++ VSCodeDebug/main.cpp
@@ -1,15 +1,17 @@
+#include <iostream>
 #include <memory>
 
-struct Type
-{
-    int do_thing() const {return data + 4;};
-    int data;
+struct Type {
+  int do_thing() const { return data + 4; };
+  int data;
 };
 
-int main()
-{
-    Type obj;
-    obj.do_thing();
-    int b = 5;
-    int a[5] = {1, 2, 3, 4,5};
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    std::cout << "Ho Hai Dang";
+  }
+  Type obj;
+  obj.do_thing();
+  int b = 5;
+  int a[5] = {1, 2, 3, 4, 5};
 }
