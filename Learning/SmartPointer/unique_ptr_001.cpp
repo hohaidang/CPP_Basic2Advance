164f00a720ab22c2d14b0bd1fcf8597cd42aa205
HO HAI DANG <hohaidang.khtn@gmail.com>
2021-01-14 14:34:45 +0900

    add

--- Learning/SmartPointer/unique_ptr_001.cpp
+++ Learning/SmartPointer/unique_ptr_001.cpp
@@ -15,6 +15,14 @@ public:
 void modifyQueue(std::unique_ptr<Entity> item) {
     
 }
+class my_class{
+    int a;
+};
+
+struct blk1 {
+    std::unique_ptr<my_class> msg;
+};
+
 
 int main() {
   std::deque<std::unique_ptr<Entity>> my_queue;
@@ -27,5 +35,8 @@ int main() {
   }
   my_queue.pop_front();
 
-  {}
+  blk1 blk;
+    
+  auto temp = std::make_unique<my_class>();
+  blk.msg = std::move(temp);
 }
