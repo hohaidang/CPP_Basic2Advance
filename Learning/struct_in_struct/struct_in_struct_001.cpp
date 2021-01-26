00c19a8a22f2b22cb18029489df56a128c4cbee4
hohaidang <hohaidang.khtn@gmail.com>
2021-01-26 16:05:26 +0900

    fix bug

--- Learning/struct_in_struct/struct_in_struct_001.cpp
+++ Learning/struct_in_struct/struct_in_struct_001.cpp
@@ -1,34 +1,45 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
 #include <iostream>
 
 
 template <int age>
 struct father {
-    int dang = 5;
-    template<typename T>
-    struct son{
-        bool restricted;
-    };
+	int dang = 5;
 
-    struct son<age> my_son;
+	template<typename T>
+	struct son {
+		bool restricted;
+	};
+
+	struct son<int> my_son;
 };
 
 template <int year>
-struct family{
-    struct father<year> fat;
+struct family {
+	struct father<year> fat;
+};
+
+struct son2 {
+	bool restricted;
 };
 
 int main() {
-    father<5> my_father;
-    my_father.dang = 15;
-    father<5>::son<int> my_son;
-    my_son.restricted = false;
+	father<5> my_father;
+	my_father.dang = 15;
+    // day la cach called 1 struct nam trong struct khac
+	father<5>::son<int> my_son; // bien nay se co kieu du lieu la father<5>::son<int> chu khong phai la son
+	my_son.restricted = false;
+	std::cout << "size of fater::son = " << sizeof(my_son) << '\n';
+	son2 dang_son; // bien nay se co kieu du lieu la son2
+	dang_son.restricted = true;
+	std::cout << "size of son2 = " << sizeof(dang_son) << '\n';
+	family<5> my_family;
+	my_family.fat.my_son.restricted = false;
+    std::cout << "size of my_family = " << sizeof(my_family.fat.my_son) << '\n';
+    std::cout << "offset of = " << offsetof(struct father<5>, my_son);
 
-    family<5> my_family;
-    my_family.fat.my_son.restricted = false;
-    
-    // my_family<5>::father.dang = 5;
-    // my_family<5>::father::son<int>::restricted = false;
-    std::cout << "my_son = " << std::boolalpha << my_son.restricted << '\n';
-    std::cout << "HelloWorld\n";
-    return 0;
+	return 0;
 }
+
