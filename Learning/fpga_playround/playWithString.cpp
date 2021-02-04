d6e947ce538cc5ecbb59ee43a1caa6ac9629c984
hohaidang <hohaidang.khtn@gmail.com>
2021-02-04 16:57:57 +0900

    add

--- Learning/fpga_playround/playWithString.cpp
+++ Learning/fpga_playround/playWithString.cpp
@@ -17,13 +17,13 @@ int main() {
 
   auto msg = Raptor::MxEMAPIMsg<Raptor::Message<decltype(a)>>(a);
 
-  // play ground
-  auto data = msg.s<0, 1>(); // tach char tu phan tu 0 length = 1
-  if(data == stringNZ<1>("D")) { // so sanh string
-    std::cout << "Equal" << std::endl;
-  } else {
-    std::cout << "Not Equal" << std::endl;
-  }
+  // play ground, phan nay phai chuyen Messge -> s thanh public
+//  auto data = msg.s<0, 1>(); // tach char tu phan tu 0 length = 1
+//  if(data == stringNZ<1>("D")) { // so sanh string
+//    std::cout << "Equal" << std::endl;
+//  } else {
+//    std::cout << "Not Equal" << std::endl;
+//  }
 
   auto dang = stringNZ<13>("Dang dep trai"); // khai bao bien
   StringN<15> issueCode = dang; // assign vao StringN
@@ -37,4 +37,18 @@ int main() {
   std::cout << "Before temp3 = " << temp3.s << '\n';
   std::cout << "After temp3 = " << temp3[0] << '\n';
 
+
+  // parseUInt
+  StringN<2> test_string(stringNZ<2>("1a"));
+  auto test_int = test_string.parseUInt();
+  std::cout << "test_int = " << std::dec << test_int.result << '\n';
+  std::cout << "test_int witdh = " << decltype(test_int.result)::width << '\n';
+  std::cout << "test_int witdh = " << test_int.result.width << '\n';
+  // width nay se tinh so bit tong cong de chua duoc so ky tu tra cho chu so
+  // vd 1 character = 4 with, 0->9
+  //    2 character = 7 with, 0->99
+  if(test_int.error) {
+    // bien error nay cho biet la trong string co character, nen se convert ra kieu int bi loi
+    std::cout << "Convert error\n";
+  }
 }
